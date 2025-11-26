fb1cf788d559f8b5ef11fe33f95764edf58f4690
Franco Barberis <francobarberissic3@gmail.com>
2025-11-25 21:19:29 -0300

    Cards layout fixed

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,31 +1,47 @@
-
-import './ProjectCard.css';
-import React from 'react';
+import "./ProjectCard.css";
+import React from "react";
 
 interface ProjectCardProps {
-    title: string;
-    description: string;
-    imageUrl: string;
-    link?: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  link?: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageUrl, link }) => (
-    <div className="project-card" style={{ backgroundImage: `url(${imageUrl})` }}>
-        <div className="project-card-content">
-            <h3 className="project-card-title">{title}</h3>
-            <p className="project-card-description">{description}</p>
-            {link && (
-                <a
-                    href={link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="project-card-link"
-                >
-                    Ver proyecto
-                </a>
-            )}
-        </div>
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  description,
+  imageUrl,
+  link,
+}) => (
+  <div className="project-card" style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className="project-card-content">
+      <h3 className="project-card-title">{title}</h3>
+      <p className="project-card-description">{description}</p>
+      <div className="project-card-links">
+        {link && (
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-card-link"
+          >
+            Ver proyecto
+          </a>
+        )}
+        {link && (
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-card-link"
+          >
+            GitHub
+          </a>
+        )}
+      </div>
     </div>
+  </div>
 );
 
 export default ProjectCard;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
     <section className="projects" id="projects">
       <h1>Proyectos</h1>
       <div className="projects-list">
-        <ProjectCard title="Wallpaper Provider" description="Portal para descargar wallpapers" imageUrl={WallpaperProviderIMG} link="https://link-to-project-1.com"></ProjectCard>
+        <ProjectCard title="Wallpaper Provider" description="Portal que obtiene wallpapers de la API de Pexels, los muestra y permite su descarga en varios formatos según se necesite (4:3, 2:3, 1:1)" imageUrl={WallpaperProviderIMG} link="https://wallpaper-provider-m2g1yreq5-francobarberis-projects.vercel.app"></ProjectCard>
         <ProjectCard title="Proyecto 2" description="Descripción del proyecto 2" imageUrl={WallpaperProviderIMG} link="https://link-to-project-2.com"></ProjectCard>
         <ProjectCard title="Proyecto 3" description="Descripción del proyecto 3" imageUrl={WallpaperProviderIMG} link="https://link-to-project-3.com"></ProjectCard>
         <ProjectCard title="Proyecto 4" description="Descripción del proyecto 4" imageUrl={WallpaperProviderIMG} link="https://link-to-project-4.com"></ProjectCard>
